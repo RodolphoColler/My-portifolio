3f44d9e7c425d2fa6cb6fac9ce7cb1c0e4754e4a
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-02-13 07:33:52 -0300

    add for each body

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,20 @@
 import { For } from 'solid-js';
 import data from '../data';
+import '../css/trybewarts.css'
 
 function Projects() {
   return (
-    <For each={ data }>{( { title } ) => 
-      <h1>{title}</h1>
+    <For each={ data }>{( { index, title, path, description, languages } ) =>
+      <section class={ title }>
+        <img src={ path } width='500px'/>
+        <aside>
+          <h1 class={ `${title}-title` }>{index}<br />{title}</h1>
+          <p class={ `${title}-description` }>{ description }</p>
+          <h2>LANGUAGES:</h2>
+          <div className="container-languages">{languages.map(e => <p>{e}</p>)}</div>
+          <button>SEE MORE</button>
+        </aside>
+      </section>
     }</For>
   )
 }
