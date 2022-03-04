65f9bd2d6c2cccc888b778f50830a70fcb44f830
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-03-04 15:46:31 -0300

    adding links for the projects

--- src/data.js
+++ src/data.js
@@ -4,14 +4,18 @@ const data = [
     title: 'trybewarts',
     path: 'https://i.imgur.com/DwPe2FH.png',
     description: 'Tryberwarts is a short project to learn how forms are builded, how use css flexbox and train my javascript logic.',
-    languages: ['html', 'css', 'javascript']
+    languages: ['html', 'css', 'javascript'],
+    website: 'https://github.com/RodolphoColler/trybewarts',
+    github: 'https://rodolphocoller.github.io/trybewarts/'
   },
   {
     index: '02.',
     title: 'fifteen-puzzle',
     path: 'https://i.imgur.com/EBeQNzy.png',
     description: 'I made fifteen-puzzle to challenge myself and get my first contact with solid.js. It was a good experience to improve my refactoring skills.',
-    languages: ['html', 'css', 'javascript', 'solid.js']
+    languages: ['html', 'css', 'javascript', 'solid.js'],
+    website: 'https://fifteen-puzzle-nine.vercel.app/',
+    github: 'https://github.com/RodolphoColler/fifteen-puzzle'
   }
 ]
 
