edc7088f544f3f36b3a112ad598022073f7f4dc2
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-03-04 15:45:52 -0300

    inserting icons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,10 +2,11 @@ import { For } from 'solid-js';
 import data from '../data';
 import '../css/trybewarts.css'
 import '../css/fifteen-puzzle.css'
+import { AiFillGithub } from 'solid-icons/ai'
 
 function Projects() {
   return (
-    <For each={ data }>{( { index, title, path, description, languages } ) =>
+    <For each={ data }>{( { index, title, path, description, languages, website, github } ) =>
       <section class={ title }>
         <img src={ path } width='500px'/>
         <aside>
@@ -13,7 +14,13 @@ function Projects() {
           <p class={ `${title}-description` }>{ description }</p>
           <h2>LANGUAGES:</h2>
           <div className={`${title}-languages`}>{languages.map(e => <p>{e}</p>)}</div>
-          <button>SEE MORE</button>
+          <div class='buttons-container'>
+            <a href={ website } target='_blank'>SEE MORE</a>
+            <a href={ github } target='_blank'>
+              <AiFillGithub size={30} class='git-icon'/>
+              <p>SEE IN GITHUB</p>
+            </a>
+          </div>
         </aside>
       </section>
     }</For>
