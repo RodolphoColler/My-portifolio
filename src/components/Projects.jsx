1919cdde22749786c3c232e913722f3eb4e3a4d7
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-03-23 19:40:36 -0300

    general adjustments

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,17 +9,17 @@ function Projects() {
   return (
     <For each={ data }>{( { index, project, path, description, languages, website, github } ) =>
       <section id={ project }>
-        <img src={ path } width='500px' data-aos="fade-right" data-aos-delay="600" />
+        <img src={ path } width='470px' data-aos="custom-fade-up" />
         <aside>
           <div class="animation-container">
             <h1 class={ `${project}-title` } data-aos="new-animation">{index}<br />{project}</h1>
           </div>
-          <p class={ `${project}-description`} data-aos="fade-up" >{ description }</p>
+          <p class={ `${project}-description`} data-aos="custom-fade-up" >{ description }</p>
           <div class="animation-container">
             <h2 data-aos="new-animation" data-aos-duration="900" data-aos-easing="ease-in-out" >LANGUAGES:</h2>
           </div>
           <div class="languages-container animation-container">
-            <For each={languages}>{ language => <p data-aos="fade-up">{language}</p> }</For>
+            <For each={languages}>{ language => <p data-aos="custom-fade-up">{language}</p> }</For>
           </div>
           <div class="animation-container">
             <div class='buttons-container' data-aos="new-animation" >
