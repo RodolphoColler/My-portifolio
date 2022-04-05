1caa63635f07b6d20f1a85d4e5f2e7966a1b205c
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-04-05 15:19:24 -0300

    some css fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,13 @@
-import '../css/about.css'
+import '../css/about.scss'
+import { AiFillGithub, AiFillLinkedin, AiOutlineInstagram } from 'solid-icons/ai';
 
 function About() {
   return (
     <>
       <section class="about">
         <aside>
-          <h1 data-aos="fade-in">And what about me?</h1>
-          <p class='my-description'>
+          <h1 data-aos="fade-in">me. <br /> what about me</h1>
+          <p class='my-description' data-aos="custom-fade-up">
             As you know my name is Rodolpho, i live in state of 
             São Paulo, Brazil. Right now i'm adding back-end to my
             list of tools, so in some time i gonna be a full-stack
@@ -16,13 +17,16 @@ function About() {
             Starting from that i'm search for a job that i can 
             contribute and be open to learn good programming.
           </p>
+          <div data-aos="fade-in">
+            <a href="https://github.com/RodolphoColler/trybewarts" target="_blank"><AiFillGithub size={30} color="#fffff"/>GITHUB</a>
+            <a href="https://www.linkedin.com/in/rodolpho-coller/" target="_blank"><AiFillLinkedin size={30} color="#fffff"/>LINKEDIN</a>
+            <a href="https://www.instagram.com/sweetie_da_isa/" target="_blank"><AiOutlineInstagram size={30} color="#fffff"/>INSTAGRAM</a>
+          </div>
         </aside>
-        <img src="https://i.imgur.com/v0r6bpJ.jpg" alt="" width="350px" />
+        <img src="https://i.imgur.com/v0r6bpJ.jpg" alt="" width="400px"/>
       </section>
     </>
   );
 }
 
 export default About;
-
-// https://i.imgur.com/FaEr7rk.jpeg
