5f1aaba3bedbe233104e8b2437dd7490a7b522f9
RodolphoColler <rodolpho.coller12@gmail.com>
2022-10-07 14:38:38 -0300

    change work role on home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Presentation() {
   return (
     <section id="home">
       <h1>Hi, Im Rodolpho</h1>
-      <h2>Front-end developer <br /> trying look outside the box</h2>
+      <h2>Full-stack developer <br /> trying look outside the box</h2>
       <h3>see my projects</h3>
       <a href="#trybewarts">
         <RiSystemArrowDownSLine size={70}/>
