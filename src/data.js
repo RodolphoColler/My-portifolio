4e690426faca28aa71cb82735500ddd547f1c056
RodolphoColler <rodolpho.coller12@gmail.com>
2025-08-10 11:59:10 -0300

    chore: mudando descrição dos projetos

--- src/data.js
+++ src/data.js
@@ -1,40 +1,23 @@
 const data = [
   {
     index: '01.',
-    project: 'trybewarts',
-    path: 'https://i.imgur.com/DwPe2FH.png',
-    description: 'Tryberwarts is a short project to learn how forms are builded, how use css flexbox and train my javascript logic.',
-    languages: ['html', 'css', 'javascript'],
-    website: 'https://rodolphocoller.github.io/trybewarts/',
-    github: 'https://github.com/RodolphoColler/trybewarts'
-  },
-  {
-    index: '02.',
     project: 'fifteen-puzzle',
     path: 'https://i.imgur.com/EBeQNzy.png',
-    description: 'I made fifteen-puzzle to challenge myself and get my first contact with solid.js. It was a good experience to improve my refactoring skills.',
-    languages: ['html', 'css', 'javascript', 'solid.js'],
+    description: 'Desenvolvi este projeto como um desafio pessoal e para conhecer um novo framework front-end. Foi uma excelente experiência para aprimorar minhas habilidades de refatoração',
+    languages: ['Html', 'Css', 'Javascript', 'Solid.js'],
     website: 'https://fifteen-puzzle-nine.vercel.app/',
     github: 'https://github.com/RodolphoColler/fifteen-puzzle'
   },
   {
-    index: '03.',
+    index: '02.',
     project: 'tfc',
     path: 'https://i.imgur.com/ed0Pv5U.png',
     description: 'TFC is a informative site about matches and football classification. It\'s the first time that i integrate backend and frontend.',
-    languages: ['node', 'express', 'typescript', 'sequelize', 'jwt'],
+    description: 'TFC é um site informativo sobre partidas e classificações de futebol. Foi a primeira aplicação full-stack que desenvolvi. ',
+    languages: ['Node', 'Express', 'Typescript', 'Sequelize', 'Jwt'],
     website: 'https://tfc-frontend.vercel.app/leaderboard',
     github: 'https://github.com/RodolphoColler/tfc-frontend'
   },
-  {
-    index: '04.',
-    project: 'blizter',
-    path: 'https://i.imgur.com/xHWmG2H.png',
-    description: 'The easy way to get in day with your financial life, Blizter is my favorite and the first project that I build everything completely from scratch.',
-    languages: ['node', 'express', 'typescript', 'prisma', 'react'],
-    website: 'https://blizter.vercel.app/',
-    github: 'https://github.com/RodolphoColler/blizter-frontend'
-  }
 ]
 
 export default data;
