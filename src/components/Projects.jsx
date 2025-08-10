e6ba74c107d3ac16a2b47f3fe3da1a0c015e1f93
RodolphoColler <rodolpho.coller12@gmail.com>
2025-08-10 11:58:38 -0300

    style: criação de um responsivo simples

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,31 +11,34 @@ function Projects() {
   return (
     <For each={ data }>{( { index, project, path, description, languages, website, github } ) =>
       <section id={ project }>
-        <img src={ path } width='470px' data-aos="custom-fade-up" />
-        <aside>
-          <div class="animation-container">
-            <h1 class={ `${project}-title` } data-aos="new-animation">{index}<br />{project}</h1>
-          </div>
-          <p class={ `${project}-description`} data-aos="custom-fade-up" >{ description }</p>
-          <div class="animation-container">
-            <h2 data-aos="new-animation" data-aos-duration="900" data-aos-easing="ease-in-out" >LANGUAGES:</h2>
-          </div>
-          <div class="languages-container animation-container">
-            <For each={languages}>{ language => <p data-aos="custom-fade-up">{language}</p> }</For>
-          </div>
-          <div class="animation-container">
-            <div class='buttons-container' data-aos="new-animation" >
-              <a href={ website } target='_blank'>
-                <CgWebsite size={30}/>
-                WEBSITE
-              </a>
-              <a href={ github } target='_blank'>
-                <AiFillGithub size={32}/>
-                SEE IN GITHUB
-              </a>
+        <div id={ `${project}-container`} >
+          <img src={ path } width='470px' data-aos="custom-fade-up" />
+          <aside>
+            <div class="animation-container">
+              <h1 class={ `${project}-title` } data-aos="new-animation">{index}<br />{project}</h1>
             </div>
-          </div>
-        </aside>
+            <p class={ `${project}-description`} data-aos="custom-fade-up" >{ description }</p>
+            <div class="animation-container">
+              <h2 data-aos="new-animation" data-aos-duration="900" data-aos-easing="ease-in-out" >LANGUAGES:</h2>
+            </div>
+            <div class="languages-container animation-container">
+              <For each={languages}>{ language => <p data-aos="custom-fade-up">{language}</p> }</For>
+            </div>
+            <div class="animation-container">
+              <div class='buttons-container' data-aos="new-animation" >
+                <a href={ website } target='_blank'>
+                  <CgWebsite size={30}/>
+                  WEBSITE
+                </a>
+                <a href={ github } target='_blank'>
+                  <AiFillGithub size={32}/>
+                  SEE IN GITHUB
+                </a>
+              </div>
+            </div>
+          </aside>
+        </div>
+
       </section>
     }</For>
   )
